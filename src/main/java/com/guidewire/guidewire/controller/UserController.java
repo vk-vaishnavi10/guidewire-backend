da80061e49956d21c8b3f02f34c9011a8ebaed12
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2026-04-04 03:26:20 +0530

    final fix remove repo

--- src/main/java/com/guidewire/guidewire/controller/UserController.java
+++ src/main/java/com/guidewire/guidewire/controller/UserController.java
@@ -1,52 +1,25 @@
 package com.guidewire.guidewire.controller;
 
 import com.guidewire.guidewire.entity.User;
-import com.guidewire.guidewire.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
-
     @PostMapping("/save-profile")
-    public User saveProfile(@RequestBody User userData) {
+    public String saveProfile(@RequestBody User userData) {
 
-        Optional<User> userOptional = userRepository.findByPhone(userData.getPhone());
+        System.out.println("User saved:");
+        System.out.println("Phone: " + userData.getPhone());
+        System.out.println("Name: " + userData.getName());
+        System.out.println("City: " + userData.getCity());
+        System.out.println("Vehicle: " + userData.getVehicle());
+        System.out.println("Platform: " + userData.getPlatform());
+        System.out.println("Experience: " + userData.getExperience());
+        System.out.println("Bank: " + userData.getBankName());
 
-        User user;
-
-        if (userOptional.isPresent()) {
-            user = userOptional.get();
-        } else {
-            user = new User();
-            user.setPhone(userData.getPhone());
-            user.setVerified(true); // OTP already verified
-        }
-
-        // 🔹 PROFILE DATA
-        user.setName(userData.getName());
-        user.setCity(userData.getCity());
-        user.setVehicle(userData.getVehicle());
-
-        if (userData.getPlatform() != null) {
-            user.setPlatform(userData.getPlatform());
-        }
-
-        user.setExperience(userData.getExperience());
-
-        // 🔥 BANK DETAILS (THIS WAS MISSING)
-        user.setBankHolder(userData.getBankHolder());
-        user.setAccountNumber(userData.getAccountNumber());
-        user.setIfsc(userData.getIfsc());
-        user.setBankName(userData.getBankName());
-
-        return userRepository.save(user);
+        return "User saved successfully";
     }
 }
