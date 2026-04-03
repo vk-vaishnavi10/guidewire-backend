ac4555b0d3c1385bf2b2e3078f556d67bf55312c
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2026-04-04 03:11:38 +0530

    fix cors

--- src/main/java/com/guidewire/guidewire/GuidewireApplication.java
+++ src/main/java/com/guidewire/guidewire/GuidewireApplication.java
@@ -2,7 +2,10 @@ package com.guidewire.guidewire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.guidewire.guidewire.repository")
@@ -10,5 +13,17 @@ public class GuidewireApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GuidewireApplication.class, args);
+    }
+	 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
