6b9226b2711486b5292075cfeede56a581c5b99f
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2026-04-04 02:20:02 +0530

    fix repo scan final

--- src/main/java/com/guidewire/guidewire/GuidewireApplication.java
+++ src/main/java/com/guidewire/guidewire/GuidewireApplication.java
@@ -2,9 +2,12 @@ package com.guidewire.guidewire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.guidewire.guidewire")
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.guidewire.guidewire.repository")
 public class GuidewireApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GuidewireApplication.class, args);
     }
